1382f1416b77f617eecd4c710accbd17b6c0ff57
LautarojDiaz <diazlauta9@gmail.com>
2023-04-04 19:49:03 -0300

    Segundo desafio

--- ProductManager.js
+++ ProductManager.js
@@ -1,12 +1,16 @@
+
 export default class ProductManager {
     #products
     #productId
-    constructor(){
+    #route
+
+    constructor(route){
         this.#products = []
         this.#productId=1
+        this.#route = route
     }
 
-    addProduct(title,description,price,thumbnail,code,stock){
+    addProduct(title, description, price, thumbnail, code, stock){
         const prodWithSameCode=this.#products.find((prod)=>prod.code===code)
         if(!title||!description||!price||!thumbnail||!code||!stock||prodWithSameCode){
             if(prodWithSameCode){
@@ -16,21 +20,45 @@ export default class ProductManager {
             }
         }else{
             let product={
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-            id:this.#productId
-        }
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock,
+                id:this.#productId
+            }
             this.#products.push(product)
-            this.#productId++  
+            this.#productId++
+
+            this.#route.save(this.#products)  
         }
     }
+
+    updateProduct(id, newProduct){
+        const index = this.#products.findIndex((prod)=>prod.id===id)
+        if(index === -1){
+            console.log("Product not found")
+        } else {
+            this.#products[index] = {...newProduct, id}
+            this.#route.save(this.#products)
+        }
+    }
+
+    deleteProduct(id){
+        const index = this.#products.findIndex((prod)=>prod.id===id)
+        if(index === -1){
+            console.log("Product not found")
+        } else {
+            this.#products.splice(index, 1)
+            this.#route.save(this.#products)
+        }
+    }
+
     getProducts(){
         return this.#products
     }
+
     getProductById(id){
         const productFindById=this.#products.find((prod)=>prod.id===id)
         if(!productFindById){
